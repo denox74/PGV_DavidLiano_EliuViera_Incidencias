e13d15363049b9266822636996dee99d2e138732
denox74 <davidlianomacias@alumno.ieselrincon.es>
2026-01-25 12:44:47 +0000

    Incidencia

--- src/main/java/com/model/Incidence.java
+++ src/main/java/com/model/Incidence.java
@@ -1,21 +1,49 @@
 package com.model;
 
+import java.time.LocalDateTime;
+
 // Clase para representar una incidencia. ID y descripción. 
 
 public class Incidence {
     private final int id;
-    private final String descripcion;
+    private String description;
+    private State state;
+    private final LocalDateTime dateTime;
+    private String userIncidence;
 
-    public Incidence(int id, String descripcion) {
+    public Incidence(int id, String description) {
         this.id = id;
-        this.descripcion = descripcion;
+        this.description = description;
+        this.state = State.OPEN;
+        this.dateTime = LocalDateTime.now();
+        this.userIncidence = "";
     }
 
     public int getId() {
         return id;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+    public String getDescription() {
+        return description;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getUserIncidence() {
+        return userIncidence;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void setUserIncidence(String userIncidence) {
+        this.userIncidence = userIncidence;
     }
 }
---
+++ src/main/java/com/model/State.java
@@ -0,0 +1,8 @@
+package com.model;
+
+public enum State {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
