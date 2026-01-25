b00e54b5dc8a7371321707c19fc3c68d4c6d4897
Escadi <eliuvl86@gmail.com>
2026-01-25 13:04:00 +0000

    Normalizer create

--- src/main/java/com/util/Normalizer.java
+++ src/main/java/com/util/Normalizer.java
@@ -2,4 +2,26 @@ package com.util;
 
 public class Normalizer {
 
+    private String normalizer(String description) {
+
+        if (description == null) {
+            return null;
+        }
+
+        String des = description.trim();
+
+        if (des.isEmpty()) {
+            return null;
+        }
+
+        if (des.length() > 200) {
+            return null;
+        }
+
+        if (!des.matches("[A-Za-z0-9 áéíóúÁÉÍÓÚñÑ.,;:()/_\\\\-]+")) {
+            return null;
+        }
+
+        return des;
+    }
 }
